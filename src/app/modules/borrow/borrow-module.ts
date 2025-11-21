5717ba222ccc645a633d1472522c75a4373601dd
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-21 23:14:24 +0800

    initial commit on borrow module

---
+++ src/app/modules/borrow/borrow-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BorrowRoutingModule } from './borrow-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BorrowRoutingModule
+  ]
+})
+export class BorrowModule { }
