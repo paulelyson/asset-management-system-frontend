3ccf4af30e849469327e53590f3dc309f46b35ca
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-21 23:13:46 +0800

    initial commit on inventory module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inventory',
+    loadChildren: () => import('./modules/inventory/inventory-module').then((m) => m.InventoryModule),
+  },
+];
---
+++ src/app/modules/inventory/inventory/inventory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrl: './inventory.component.css',
+  standalone: false,
+})
+export class InventoryComponent {
+
+}
