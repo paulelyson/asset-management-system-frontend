3d5382afddf9ecb0ed3b7e2ed700aa164368db95
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-22 15:13:34 +0800

    initial commit on equipment filter

---
+++ src/app/modules/shared/equipment-filter/equipment-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-filter',
+  imports: [],
+  templateUrl: './equipment-filter.component.html',
+  styleUrl: './equipment-filter.component.css',
+})
+export class EquipmentFilterComponent {
+
+}
