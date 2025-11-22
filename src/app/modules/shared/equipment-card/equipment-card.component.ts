651b2901df39f18d43495301054c719698c51e65
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-22 14:30:31 +0800

    update on equipment card

--- src/app/modules/shared/equipment-card/equipment-card.component.ts
+++ src/app/modules/shared/equipment-card/equipment-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+type CardSize = 'sm' | 'md' | 'lg';
+type CardType = 'default' | 'primary' | 'secondary' | 'accent' | 'success' | 'warning' | 'danger';
+type CardShade = 'default' | 'light';
 
 @Component({
   selector: 'app-equipment-card',
-  imports: [ButtonComponent],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './equipment-card.component.html',
   styleUrl: './equipment-card.component.css',
 })
 export class EquipmentCardComponent {
-
+  @Input() size: CardSize = 'md';
+  @Input() type: CardType = 'primary';
+  @Input() shade: CardShade = 'default';
+  @Input() title: string = '';
+  @Input() descriptions: string[] = [];
+  @Input() icon: string = 'calendar_today';
 }
