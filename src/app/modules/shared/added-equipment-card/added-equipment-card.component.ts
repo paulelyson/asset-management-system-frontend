6705a26a690f8af7a05666b6a3e4a1d8da27634b
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-22 16:18:05 +0800

    initial commit on added equipment card

---
+++ src/app/modules/shared/added-equipment-card/added-equipment-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-added-equipment-card',
+  imports: [],
+  templateUrl: './added-equipment-card.component.html',
+  styleUrl: './added-equipment-card.component.css',
+})
+export class AddedEquipmentCardComponent {
+
+}
