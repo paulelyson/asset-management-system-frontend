75a29ebf0f6c7eb5b3fbb33f8786eaca8be019ec
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-23 15:00:02 +0800

    update on equipment detail dialog

---
+++ src/app/modules/shared/equipment-detail-dialog/equipment-detail-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+const DEFAULT_IMG = './assets/equipment_default_image.png';
+
+@Component({
+  selector: 'app-equipment-detail-dialog',
+  imports: [MatDividerModule],
+  templateUrl: './equipment-detail-dialog.component.html',
+  styleUrl: './equipment-detail-dialog.component.css',
+})
+export class EquipmentDetailDialogComponent {
+  DEFAULT_IMG: string = '../../../../assets/equipment_default_image.png';
+}
--- src/app/services/dialog.service.ts
+++ src/app/services/dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../modules/shared/dialog/dialog.component';
+import { EquipmentDetailDialogComponent } from '../modules/shared/equipment-detail-dialog/equipment-detail-dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { DialogComponent } from '../modules/shared/dialog/dialog.component';
 export class DialogService {
   constructor(private dialog: MatDialog) {}
   openDialog(): void {
-    const dialogRef = this.dialog.open(DialogComponent, {});
+    const dialogRef = this.dialog.open(EquipmentDetailDialogComponent, {});
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
