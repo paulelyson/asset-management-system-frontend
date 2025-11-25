c01b4f6a9b47b3a7a3603427354ee2cb397d2719
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-25 16:19:58 +0800

    added getequipment in inventory page

--- src/app/modules/inventory/inventory/inventory.component.ts
+++ src/app/modules/inventory/inventory/inventory.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { DialogService } from '../../../services/dialog.service';
+import { EquipmentService } from '../../../services/equipment.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { IEquipment } from '../../../models/Equipment';
 
 @Component({
   selector: 'app-inventory',
@@ -8,12 +11,33 @@ import { DialogService } from '../../../services/dialog.service';
   standalone: false,
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InventoryComponent {
-  constructor(private dialogService: DialogService) {}
+export class InventoryComponent implements OnInit {
   sidenav_opened: boolean = true;
+  equipment: WritableSignal<IEquipment[]> = signal([]);
+  constructor(
+    private dialogService: DialogService,
+    private equipmentService: EquipmentService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: Params) => this.queryParamsHandling(params));
+  }
+
+  getEquipment(): void {
+    this.equipmentService.getEquipment().subscribe({
+      next: (resp) => {
+        this.equipment.set(resp);
+        console.log('ressspp', this.equipment());
+      },
+    });
+  }
 
   openDialog() {
-    console.log('dancing queen')
     this.dialogService.openDialog('equipment-detail');
+  }
+
+  queryParamsHandling(params: Params): void {
+    this.getEquipment()
   }
 }
