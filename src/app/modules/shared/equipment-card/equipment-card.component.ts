99afae0154dfd1cfa1987d8d98888d10d6aa027f
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-30 18:41:33 +0800

    fix some css on equipment card and added equipment details

--- src/app/modules/shared/equipment-card/equipment-card.component.ts
+++ src/app/modules/shared/equipment-card/equipment-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { IEquipment } from '../../../models/Equipment';
+import { IconComponent } from '../icon/icon.component';
 
 type CardSize = 'sm' | 'md' | 'lg';
 type CardType = 'default' | 'primary' | 'secondary' | 'accent' | 'success' | 'warning' | 'danger';
@@ -9,15 +11,17 @@ type CardShade = 'default' | 'light';
 
 @Component({
   selector: 'app-equipment-card',
-  imports: [CommonModule, MatIconModule, ButtonComponent],
+  imports: [CommonModule, MatIconModule, ButtonComponent, IconComponent],
   templateUrl: './equipment-card.component.html',
   styleUrl: './equipment-card.component.css',
 })
 export class EquipmentCardComponent {
+  @Input() equipment!: IEquipment;
   @Input() size: CardSize = 'md';
   @Input() type: CardType = 'primary';
   @Input() shade: CardShade = 'default';
   @Input() title: string = '';
   @Input() descriptions: string[] = [];
   @Input() icon: string = 'calendar_today';
+  default_img = 'https://placehold.co/60?text=No+Image&font=poppins';
 }
