b28644c6e364f791b43fd5cc9ee1664fadcd3ccc
paulelyson <paulelyson.villaceran@gmail.com>
2025-12-09 22:14:58 +0800

    initial commit on user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IUser } from '../models/User';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+interface ApiResponse {
+  data: IUser[];
+  message: string;
+  success: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<IUser[]> {
+    return this.http.get<ApiResponse>(environment.api_url + '/api/user', {}).pipe(
+      map((resp) => resp.data),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(err: HttpErrorResponse) {
+    return throwError(() => new Error(err.error.message || err.error));
+  }
+}
