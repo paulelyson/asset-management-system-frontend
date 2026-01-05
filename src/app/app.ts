d41654e26af2150472cca4b1670a8d6c646aacb5
paulelyson <paulelyson.villaceran@gmail.com>
2026-01-05 17:25:42 +0800

    remove side nav, mat-sidenav override

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideNavigationComponent } from './modules/shared/side-navigation/side-navigation.component';
 import { HeaderComponent } from './modules/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideNavigationComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
