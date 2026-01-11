ba476411174bbfbf420a33c05e15b6ffa80e3d38
paulelyson <paulelyson.villaceran@gmail.com>
2026-01-11 16:10:27 +0800

    update on row header

--- src/app/modules/shared/row-header/row-header.component.ts
+++ src/app/modules/shared/row-header/row-header.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+export interface IRowHeader {
+  id: number; // fix track by identity warning
+  type: 'badge' | 'text';
+  text: string;
+  span: 'wide' | 'mid' | 'narrow';
+}
 
 @Component({
   selector: 'app-row-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './row-header.component.html',
   styleUrl: './row-header.component.css',
 })
 export class RowHeaderComponent {
-
+  @Input() headers: IRowHeader[] = [
+    {
+      id: 0,
+      type: 'text',
+      text: 'Equipment',
+      span: 'wide',
+    },
+    {
+      id: 1,
+      type: 'text',
+      text: 'Class',
+      span: 'mid',
+    },
+    {
+      id: 2,
+      type: 'text',
+      text: 'Borrower',
+      span: 'mid',
+    },
+     {
+      id: 2,
+      type: 'text',
+      text: 'Total # Requested',
+      span: 'narrow',
+    },
+    {
+      id: 3,
+      type: 'text',
+      text: 'Status',
+      span: 'mid',
+    },
+    {
+      id: 4,
+      type: 'text',
+      text: 'Date Requested',
+      span: 'narrow',
+    },
+  ];
 }
