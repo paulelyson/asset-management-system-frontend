1c5984bd1ba686be0eb5d322d873579d5b2279e6
paulelyson <paulelyson.villaceran@gmail.com>
2026-01-15 09:58:51 +0800

    update on vertical stepper design

--- src/app/modules/shared/vertical-stepper/vertical-stepper.component.ts
+++ src/app/modules/shared/vertical-stepper/vertical-stepper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-stepper',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './vertical-stepper.component.css',
 })
 export class VerticalStepperComponent {
-
+ @Input() contents: string[] = ['asdfs', 'bar', 'foo', 'bear']
 }
