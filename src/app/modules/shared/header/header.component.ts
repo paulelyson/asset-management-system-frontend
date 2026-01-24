c91437b1d0ae6bcfec880e19acc2c807392fe802
paulelyson <paulelyson.villaceran@gmail.com>
2026-01-24 13:12:14 +0800

    update on header

--- src/app/modules/shared/header/header.component.ts
+++ src/app/modules/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { IconComponent } from '../icon/icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatMenuModule, IconComponent],
+  imports: [MatMenuModule, IconComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
