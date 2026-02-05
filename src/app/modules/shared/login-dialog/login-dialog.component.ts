58727b5ebfaff3c849441c24df07134e2498de50
paulelyson <paulelyson.villaceran@gmail.com>
2026-02-05 16:29:52 +0800

    update on login ui

--- src/app/modules/shared/login-dialog/login-dialog.component.ts
+++ src/app/modules/shared/login-dialog/login-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-login-dialog',
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login-dialog.component.css',
 })
