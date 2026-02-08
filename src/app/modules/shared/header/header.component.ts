a9341fae28d6f7dd153c471e195a039649b0582f
paulelyson <paulelyson.villaceran@gmail.com>
2026-02-08 12:42:59 +0800

    initial commit on menu

--- src/app/modules/shared/header/header.component.ts
+++ src/app/modules/shared/header/header.component.ts
@@ -4,10 +4,11 @@ import { IconComponent } from '../icon/icon.component';
 import { RouterLink } from '@angular/router';
 import { DialogService } from '../../../services/dialog.service';
 import { AvatarComponent } from '../avatar/avatar.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatMenuModule, IconComponent, RouterLink, AvatarComponent],
+  imports: [MatMenuModule, IconComponent, RouterLink, AvatarComponent, MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
