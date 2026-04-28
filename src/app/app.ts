b7c696af920d248c5cbfba031d5d0a664f4d9278
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-28 22:38:13 +0800

    feat(header): refactor header component structure and styling for improved UI consistency feat(homepage): enhance homepage background with gradient overlay for better visual appeal refactor(app): simplify app layout by removing side navigation components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './modules/shared/header/header.component';
-import { SideNavigationComponent } from './modules/shared/side-navigation/side-navigation.component';
-import { SideMenuComponent } from './modules/shared/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SideMenuComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/modules/shared/header/header.component.ts
+++ src/app/modules/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, computed, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { IconComponent } from '../icon/icon.component';
 import { RouterLink } from '@angular/router';
@@ -6,24 +6,28 @@ import { DialogService } from '../../../services/dialog.service';
 import { AvatarComponent } from '../avatar/avatar.component';
 import { MenuComponent } from '../menu/menu.component';
 import { AuthService, TokenData } from '../../../services/auth.service';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatMenuModule, RouterLink, AvatarComponent, MenuComponent],
+  imports: [MatMenuModule, RouterLink, AvatarComponent, MenuComponent, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
+  @Input() isTransparent: boolean = true;
+
   sidenav_opened: boolean = false;
   showAvatarMenu: boolean = false;
   isLoggedIn: WritableSignal<boolean> = signal(false);
   user: WritableSignal<TokenData | null> = signal(null);
-
   subtitles = computed(() => {
     let contents: string[] = [];
-    const roles = this.user()?.roles.map((role) => `${role.role.toUpperCase()} - ${role.department.code}`) || [];
-    contents.push('ID NUMBER: '+ this.user()?.idNumber || '')
-    contents = contents.concat(roles)
+    const roles =
+      this.user()?.roles.map((role) => `${role.role.toUpperCase()} - ${role.department.code}`) ||
+      [];
+    contents.push('ID NUMBER: ' + this.user()?.idNumber || '');
+    contents = contents.concat(roles);
     return contents;
   });
 
