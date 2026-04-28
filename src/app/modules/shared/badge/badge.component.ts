94ef69985772bea83de4c7b43cde74900fa19a38
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-28 23:17:49 +0800

    feat(badge): refactor badge component to use variant prop and update icon handling

--- src/app/modules/shared/badge/badge.component.ts
+++ src/app/modules/shared/badge/badge.component.ts
@@ -1,33 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-
-type BadgeSize = 'sm' | 'md' | 'lg';
-export type BadgeType = 'primary' | 'success' | 'warning' | 'danger' | 'default';
-interface IconType {
-  type: BadgeType;
-  icon: string;
-}
-
-interface BadgeTypeSetting {
-  value: string;
-  type: BadgeType;
-}
+import { Size, Variant } from '../../../models/ui/common-config.model';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-badge',
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './badge.component.html',
   styleUrl: './badge.component.css',
 })
 export class BadgeComponent {
-  @Input() size: BadgeSize = 'md';
-  @Input() type: BadgeType = 'default';
+  @Input() size: Size = 'sm';
+  @Input() variant: Variant = 'neutral';
   @Input() hasBadgeIcon: boolean = false;
   @Input() hasCloseIcon: boolean = false;
   @Input() clickable: boolean = true;
   @Output() closed: EventEmitter<string> = new EventEmitter<string>();
-  iconlist: IconType[] = [];
 
   onClosed(): void {
     this.closed.emit();
