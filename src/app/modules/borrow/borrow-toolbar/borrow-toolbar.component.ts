ce6893a38745411fca7f6847b6fc047885d4cfd8
paulelyson <paulelyson.villaceran@gmail.com>
2026-04-30 16:06:26 +0800

    feat(borrow-toolbar): implement borrow toolbar layout with filter badges and buttons

--- src/app/modules/borrow/borrow-toolbar/borrow-toolbar.component.ts
+++ src/app/modules/borrow/borrow-toolbar/borrow-toolbar.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FilterDisplay } from '../../../models/ui/common-config.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { DialogService } from '../../../services/dialog.service';
+import { NavigationExtras, Router } from '@angular/router';
+import { BadgeComponent } from '../../shared/badge/badge.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-borrow-toolbar',
-  imports: [],
+  imports: [BadgeComponent, ButtonComponent, ReactiveFormsModule, InputComponent],
   templateUrl: './borrow-toolbar.component.html',
   styleUrl: './borrow-toolbar.component.css',
 })
 export class BorrowToolbarComponent {
+  @Input() filters: FilterDisplay[] = [];
+  @Input() department: string = '';
+  @Output() toggleBorrowForm: EventEmitter<boolean> = new EventEmitter<boolean>();
+  searchControl = new FormControl('');
+  url: string = '';
+  constructor(
+    private dialogService: DialogService,
+    private router: Router,
+  ) {
+    this.url = this.router.url.split('?')[0];
+    this.searchControl.valueChanges.pipe(debounceTime(800)).subscribe(() => this.onSearch());
+  }
+  openFilterDialog() {
+    this.dialogService.openEquipmentFilterDialog(this.department);
+  }
 
+  clearFilter(): void {
+    this.router.navigate([this.url]);
+  }
+
+  onBadgeClosed(filter: FilterDisplay): void {
+    let navigationExtras: NavigationExtras = {
+      queryParams: { [filter.field]: null },
+      queryParamsHandling: 'merge',
+    };
+    this.router.navigate([this.url], navigationExtras);
+  }
+
+  onToggleBorrowForm() {
+    this.toggleBorrowForm.emit(true);
+  }
+
+  onSearch(): void {
+    let navigationExtras: NavigationExtras = {
+      queryParams: { page: 1, search: this.searchControl.value },
+      queryParamsHandling: 'merge',
+    };
+    this.router.navigate([this.url], navigationExtras);
+  }
 }
