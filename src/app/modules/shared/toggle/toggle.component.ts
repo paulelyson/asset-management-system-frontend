fb92724c564afdd550436c5395a2b366c6098e9b
paulelyson <paulelyson.villaceran@gmail.com>
2026-05-03 16:03:26 +0800

    feat(toggle): add badge functionality and styling to toggle component

--- src/app/modules/shared/toggle/toggle.component.ts
+++ src/app/modules/shared/toggle/toggle.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-toggle',
-  imports: [MatSlideToggleModule, FormsModule, MatTooltipModule],
+  imports: [MatSlideToggleModule, FormsModule, MatTooltipModule, MatBadgeModule],
   templateUrl: './toggle.component.html',
   styleUrl: './toggle.component.css',
 })
@@ -13,8 +14,11 @@ export class ToggleComponent {
   @Input() isChecked: boolean = false;
   @Input() label: string = '';
   @Input() labelPosition: 'before' | 'after' = 'before';
-  @Input() tooltip: string = ''
+  @Input() tooltip: string = '';
   @Output() toggle = new EventEmitter<boolean>();
+
+  badge = input<string>('0');
+  hideBadge = computed(() => this.badge() == '0');
 
   onToggleChange(event: boolean) {
     this.toggle.emit(event);
